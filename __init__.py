3820249aeebafb370828b34d20d368cba1306967
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2021-06-20 12:48:46 +0200

    Added send messages

--- __init__.py
+++ __init__.py
@@ -45,15 +45,61 @@ class UbuMessagesSkill(MycroftSkill):
             id_convers[util.reorder_name(list(conver.get_members().values())[
                                          0].get_fullname()).lower()] = conver.get_conversation_id()
         bests = process.extractBests(persona, list(id_convers.keys()), scorer=fuzz.partial_ratio, score_cutoff=75)
-        if len(bests) == 0:
-            self.speak("No he encontrado a nadie.")
+        bests_list = [x[0] for x in bests]
+        self.select_person(persona, bests_list, id_convers, True)
+
+    def select_person(self, person, person_list, person_id, from_conversations):
+        if len(person_list) == 0:
+            self.speak("No he encontrado a nadie")
+            wait_while_speaking()
+            if from_conversations:
+                self.message_from_courses(person)
+            return
+        if len(person_list) > 1:
+            person_list.append("No está en la lista") # self.translate?
+            self.speak("Hay varias posibles coincidencias:")
+            wait_while_speaking()
+            sel = self.ask_selection(person_list, numeric=True)
+            if sel is None:
+                return
+            if sel == "No está en la lista":
+                if from_conversations:
+                    self.message_from_courses(person)
+                return
+            else:
+                bests_list = [sel]
         else:
-            for (match, score) in bests:
-                self.speak(str(match) + str(score))
-                wait_while_speaking()
-                if match in id_convers:
-                    self.speak(id_convers[match])
-                    wait_while_speaking()
+            yn = self.ask_yesno("He encontrado a " + person_list[0] + ". ¿Es correcto?")
+            if yn == "no":
+                if from_conversations:
+                    self.message_from_courses(person)
+                return
+        self.send_message(person_id[bests_list[0]])
+    
+    def message_from_courses(self, person):
+        course = self.get_response("¿A qué curso va esa persona?")
+        courses = self.ws.get_user_courses()
+        course_names = {}
+        for c in courses:
+            course_names[c.get_id()] = c.get_name()
+        best_course = process.extractOne(course, course_names, scorer=fuzz.partial_ratio, score_cutoff=75)
+        if best_course is not None:
+            participants = self.ws.get_participants_by_course(best_course)
+            id_person = {}
+            for p in participants:
+                id_person[util.reorder_name(p.get_fullname())] = p.get_id()
+            bests = process.extractBests(person, list(id_person.keys()), scorer=fuzz.partial_ratio, score_cutoff=75)
+            bests_list = [x[0] for x in bests]
+            self.select_person(person, bests_list, id_person, False)
+
+    def send_message(self, person_id, from_conversations):
+        message = self.get_response("Dime el mensaje")
+        yn = self.ask_yesno("He entendido " + message + "¿Es correcto?")
+        if yn != "no":
+            if from_conversations:
+                self.ws.send_message_to_conversation(message, person_id)
+            else:
+                self.ws.send_message_to_user(message, person_id)
 
     def stop(self):
         self.request_stop = True
